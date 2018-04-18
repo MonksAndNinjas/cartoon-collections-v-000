f59ae81e1f11d90f3e5844f8b3054e837ea1a46c
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-18 18:48:03 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ end
 def summon_captain_planet(veggies)# code an argument here
   # Your code here
   veggies.collect do |veggie|
-    veggie = veggie.capitalize}"!"
+    "#{veggie.capitalize}!"
   end
   return veggies
 end
