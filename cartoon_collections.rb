cbc5511bd671ca064880ca91d8fa26dc78d1f3f0
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-18 18:31:51 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,5 +17,3 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
-
-roll_call_dwarves(dwarves) {|dwarf| dwarf.each_with_index}
