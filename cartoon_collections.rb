6ff3d47bc11ac570bd0ed51e63055ef0838c06e7
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-18 18:18:45 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
   i = 0
   while i < dwarves.size
-    puts "#{i+1}.*#{dwarves}"
+    yield(dwarves[i])
     i += 1
   end
 end
